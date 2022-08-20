b3a1e41789b4684c14489177368f66b8cc7e8aee
ATIQUR RAHMAN <abulbasharatiqur@gmail.com>
2022-08-20 17:26:42 +0600

    bubble sort

--- Array/bubble_sort.c
+++ Array/bubble_sort.c
@@ -1,51 +1,52 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
     printf("Enter array size: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
     printf("Enter array elements: ");
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);
     }
     printf("Your entered array elements: ");
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ",arr[i]);
+        printf("%d ", arr[i]);
     }
     // using while loop
-//    int i=0;
-//    while(i<n-1){
-//        int j=0;
-//        while(j<n-1-i){
-//            if(arr[j]>arr[j+1]){
-//                int swap = arr[j];
-//                arr[j] = arr[j+1];
-//                arr[j+1] = swap;
-//            }
-//            j++;
-//        }
-//        i++;
-//    }
-
+    //    int i=0;
+    //    while(i<n-1){
+    //        int j=0;
+    //        while(j<n-1-i){
+    //            if(arr[j]>arr[j+1]){
+    //                int swap = arr[j];
+    //                arr[j] = arr[j+1];
+    //                arr[j+1] = swap;
+    //            }
+    //            j++;
+    //        }
+    //        i++;
+    //    }
 
     // using for loop
-    for(int i=0; i<n-1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for(int j=0; j<n-1-i; j++){
-            if(arr[j]>arr[j+1]){
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 int swap = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = swap;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
             }
         }
     }
     printf("Finally, sorted array elements: ");
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ",arr[i]);
+        printf("%d ", arr[i]);
     }
     return 0;
 }
--- Array/bubble_sort_book.c
+++ Array/bubble_sort_book.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-void bubbleSort(int *data, int n) {
-    for (int k = 1; k <= n - 1; k++) {
+void bubbleSort(int *data, int n)
+{
+    for (int k = 1; k <= n - 1; k++)
+    {
         int ptr = 0;
-        while (ptr <= n - k) {
-            if (data[ptr] > data[ptr + 1]) {
+        while (ptr <= n - k)
+        {
+            if (data[ptr] > data[ptr + 1])
+            {
                 int temp = data[ptr];
                 data[ptr] = data[ptr + 1];
                 data[ptr + 1] = temp;
@@ -14,7 +18,8 @@ void bubbleSort(int *data, int n) {
     }
 }
 
-int main() {
+int main()
+{
     int arr[5] = {5, 4, 3, 2, 1};
     bubbleSort(arr, 5);
     return 0;
