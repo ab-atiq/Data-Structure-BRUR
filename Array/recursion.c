cce573984eb6980f4bc73c5b3d00ed1fc4cdcd2d
ATIQUR RAHMAN <abulbasharatiqur@gmail.com>
2022-08-21 08:53:30 +0600

    fibonacci, factorial, tawer of hanoi

--- Array/recursion.c
+++ Array/recursion.c
@@ -1,18 +1,26 @@
+// fibonacci, factorial, tower of hanoi
 #include <stdio.h>
 
-int fibonacci(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
+int fibonacci(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int factorial(int n) {
-    if (n == 0) return 1;
+int factorial(int n)
+{
+    if (n == 0)
+        return 1;
     return n * factorial(n - 1);
 }
 
-void tower_of_hanoi(int n, char from, char aux, char to) {
-    if (n == 1) {
+void tower_of_hanoi(int n, char from, char aux, char to)
+{
+    if (n == 1)
+    {
         printf("Move disk 1 from %c to %c\n", from, to);
         return;
     }
@@ -21,7 +29,8 @@ void tower_of_hanoi(int n, char from, char aux, char to) {
     tower_of_hanoi(n - 1, aux, from, to);
 }
 
-int main() {
+int main()
+{
     printf("%d\n", fibonacci(10));
     printf("%d\n", factorial(10));
     tower_of_hanoi(4, 'A', 'B', 'C');
